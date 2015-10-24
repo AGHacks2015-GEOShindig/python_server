57954c8abce9662b67a6d306e04c0bc44e6e6fd2
Andrzej Kwak <andy.kwaq@gmail.com>
2015-10-24 11:28:42 +0200

    api for web

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+# encoding: UTF-8
 from flask import Flask
 from flask.ext.elasticsearch import FlaskElasticsearch
 from flask.ext.cors import CORS
 from flask import request
 from flask import jsonify
+from pprint import pprint
 
 
 
@@ -17,23 +19,34 @@ es.init_app(app)
 
 @app.route('/search/')
 def search():
-	city = request.args.get('city', '')
-	distance = request.args.get('distance', '')
+    distance = request.args.get('distance')
+    city = request.args.get('city')
 
-	# city_search = es.search(index="fb_events", body={"size":1, "query": {'match': {'city': 'wroclaw'}}})
-	# city_cord = city_search['geo_cord']
+    print city
+    print distance
 
-	query = {
-  "query": {
-    "filtered": {
-    "query":  { "match": { "city": "wroclaw" }},
-    "filter": { "geo_distance": { "distance": "400km", "geo_cord": {"lat": 15.2333,"lon": 52.70}}}
+    if not distance:
+        distance = "5"
+    if not city: 
+        city = u'kraków'
+
+    city_search = es.search(index="fb_events", body={"size":1, "query": {'match': {'city': city}}})
+    pprint(city_search)
+    city_cord = city_search['hits']['hits'][0]['_source']['place']['location']['geo_cord']
+    pprint(city_search)
+    city_cord = city_cord.split(',')
+    print city_cord
+    distance += "km"
+    query = {
+        "size":100, "query": {
+            "filtered": {
+                "query":  { "match": { "city": city }},
+                "filter": { "geo_distance": { "distance": distance, "geo_cord": {"lat": city_cord[0],"lon": city_cord[1]}}}
+            }
+       }
     }
-  }
-}
-	res = es.search(index="fb_events", body=query)
-	print res
-	return jsonify(**res)
+    res = es.search(index="fb_events", body=query)
+    return jsonify(res['hits'])
 
 
 
